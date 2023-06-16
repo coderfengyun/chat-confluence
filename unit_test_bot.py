3df3b6581e03ca67560719f76f7e0617a19122b0
chentienan <chentienan@CTN-MBP.local>
2023-06-16 15:36:36 +0800

    ADD: test case generator

---
+++ unit_test_bot.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from streamlit_chat import message
+from unit_test_generator import unit_tests_from_function
+
+PAGE_TITLE: str = "Java Unit Test Generator"
+PAGE_ICON: str = "🤖"
+
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON, layout="wide")
+st.title("Java Unit Test Generator")
+
+if "console_text" not in st.session_state:
+    st.session_state["console_text"] = ""
+
+col1, col2, col3 = st.columns([4,1,4])
+
+with col1:
+    st.markdown("Enter your java function here:")
+    function_to_test = col1.text_area("enter your java function", height=500, label_visibility="collapsed")
+
+with col2:
+    st.write("\n\n\n\n\n\n\n\n")
+    submit_clicked = col2.button("Generate Test Cases", key="generationSubmit")
+
+with col3:
+    st.markdown("Generated test cases:")
+    col3_place_holder = col3.empty()
+    col3_place_holder.markdown(f'''
+<div style="height:500px; overflow-y:scroll; border: 1px solid #e0e0e0; padding: 10px; border-radius: 5px;">
+</div>
+''', unsafe_allow_html=True)
+
+row2_place_holder = st.empty()
+
+
+
+def print_to_console(text: str, end: str):
+    st.session_state["console_text"] += text
+    console_text = st.session_state["console_text"]
+    markdown_container_1 = f'''
+<div id="scrollContainer"; style="background-color: #f0e68c; padding: 10px; border-radius: 5px; height:150px; overflow-y:scroll;">
+    {console_text}
+</div>
+'''
+    row2_place_holder.markdown(markdown_container_1, unsafe_allow_html=True)
+
+if submit_clicked:
+    with col2:
+        with st.spinner("Generating..."):
+            generated = unit_tests_from_function(function_to_test, print_text=True, print_function=print_to_console, approx_min_cases_to_cover=10)
+            markdown_container = f'''
+<div style="height:500px; overflow-y:scroll; border: 1px solid #e0e0e0; padding: 10px; border-radius: 5px;">
+    {generated}
+</div>
+'''
+            col3_place_holder.markdown(markdown_container, unsafe_allow_html=True)
